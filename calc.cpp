90911c8281b3b90da31b494cd067ec0112df5800
Rebecca Turner <637275@gmail.com>
2017-11-18 16:40:15 -0500

    compiles!!

--- calc.cpp
+++ calc.cpp
@@ -13,8 +13,8 @@ public:
 protected:
 	double operate() {
 		double dleft, dright;
-		dleft = std::get<double>(left);
-		dleft = std::get<double>(right);
+		dleft = std::get<double>(*left);
+		dleft = std::get<double>(*right);
 		switch(binoperator) {
 		case(Operator::add):
 			return dleft + dright;
@@ -27,44 +27,45 @@ protected:
 		}
 	}
 
-	double evaluate_side(Operand op) {
+	double evaluate_side(Operand* op) {
 		// so what i WANT this to do is evaluate the operator, destroying
 		// the Operation field and replacing it with a double in the
 		// variant.  what it does is, im pretty sure, not that
-		return std::holds_alternative<Operation>(op)
+		return std::holds_alternative<Operation>(*op)
 			// op is an operation
-			? std::get<Operation>(op).evaluate()
+			? std::get<Operation>(*op).evaluate()
 			// op is a double
-			: std::get<double>(op);
+			: std::get<double>(*op);
 	}
+
 public:
-	Operand left;
-	Operand right;
+	Operand* left;
+	Operand* right;
 	Operator binoperator;
 
 	Operation(std::string expr) {
 	}
 
 	Operation(Operand left_, Operand right_, Operator binoperator_) {
-		left        = &left_;
-		right       = &right_;
-		binoperator = &binoperator_;
+		Operand left  = left_;
+		Operand right = right_;
+		binoperator   = binoperator_;
 	}
 
-	//~Operation() {
-		//delete &left;
-		//delete &right;
-	//}
+	~Operation() {
+		delete left;
+		delete right;
+	}
 
 	double evaluate() {
-		left  = evaluate_side(left);
-		right = evaluate_side(left);
+		//left  = evaluate_side(left);
+		//right = evaluate_side(right);
 		return operate();
 	}
 };
 
 int main(int argc, const char** argv) {
-	Operation op = new Operation(3, 4, Operation::Operator::times);
-	std::cout << "Evaluating 3 * 4: " << op.evaluate();
+	//Operation op = new Operation(3, 4, Operation::Operator::multiply);
+	//std::cout << "Evaluating 3 * 4: " << op.evaluate();
 	return 0;
 }
